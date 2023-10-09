a94b53741ebf6c31d4602b2a5bf64e3a93224cd3
mickle <mickle.develop@gmail.com>
2023-10-09 16:45:40 +0300

    imported express. app is express?:)

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-// import { requestHandler } from './routes';
+const express = require('express');
 
-const http = require('http');
+const app = express();
 
-const handler = require('./routes').requestHandler;
+app.use('/', (req, res, next) => {
+  console.log('This runs always!');
+  next();
+});
 
-const server = http.createServer(handler);
+app.use('/add-product', (req, res, next) => {
+  console.log(
+    'this middleware applies to only "/add-products", so we do not need to call next to go to the "/"'
+  );
+  res.send('<h1>I love NODE JS and adding products=)</h1>');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  console.log(
+    'this middleware applies to all routes, because they all start with "/". So we place it in the end, so all other paths are checked first'
+  );
+  console.log('this only runs if we are not visiting paths written above');
+  res.send('<h1>I love NODE JS</h1>');
+});
+
+app.listen(3000);
